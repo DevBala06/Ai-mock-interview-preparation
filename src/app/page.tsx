5bd36cd25d2112d902616e99e7fd4bc125b03633
DevBala06 <160559000+DevBala06@users.noreply.github.com>
2024-08-15 12:15:29 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,7 +117,7 @@ const Home: React.FC = () => {
           <div className="flex flex-col justify-center items-center w-[96%]  mx-auto">
             {/* <h1>Testimonials</h1> */}
           <h1 className="xl:text-4xl xl:font-black max-xl:text-3xl max-xl:font-extrabold max-md:text-xl max-md:font-extrabold ">Reviews that speak volumes.</h1>
-          <p className="xl:text-lg xl:font-bold mt-4 max-xl:text-lg max-xl:font-bold max-md:text-base max-md:font-bold max-md:px-5">Don't just take our word for it, hear what people have to say about us.</p>
+          <p className="xl:text-lg xl:font-bold mt-4 max-xl:text-lg max-xl:font-bold max-md:text-base max-md:font-bold max-md:px-5">{"Don't just take our word for it, hear what people have to say about us."}</p>
             </div>
           </div>
           <div className="h-3/4"><Testimonials/></div>
