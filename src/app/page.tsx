e36dd9d65cb66e2713cf5fff3826d0487549677f
DevBala06 <160559000+DevBala06@users.noreply.github.com>
2024-08-21 21:26:33 +0530

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,43 +10,64 @@ import Footer from "@/components/Footer";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import Testimonials from "@/components/Testimonials";
 import Features from "@/components/Features";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Home = () => {
   const { cursorRef } = useCursor();
   const elem = useRef<HTMLDivElement>(null);
+  const [sectionHeight, setSectionHeight] = useState(0);
   const { contextSafe } = useGSAP();
   
   useEffect(() => {
+    const calculateHeight = () => {
+      if (elem.current) {
+        const rect = elem.current.getBoundingClientRect(); // Get the section height
+        return rect.height;
+      }
+      return 1000; // Fallback value
+    };
+    
+    const updateHeight = () => {
+      const height = calculateHeight();
+      setSectionHeight(height);
+    };
+
+    updateHeight();
+    
     gsap.registerPlugin(ScrollTrigger);
+  
+    // Create ScrollTrigger instance
     ScrollTrigger.create({
-      id:"cardsScroll",
+      id: "cardsScroll",
       trigger: elem.current,
       pin: true,
       start: "top top",
-      end: ()=> `+=2500vh`, 
+      end: () => `+=${sectionHeight}px`, 
       scrub: true,
-      invalidateOnRefresh:true,
+      invalidateOnRefresh: true,
+    });
+  
+    // Debounced resize event handler
+    const resizeObserver = new ResizeObserver(() => {
+      updateHeight();
+      ScrollTrigger.refresh(true); // Force a full refresh after recalculating height
     });
 
-    
-  let timeoutId:any;
-  const resizeObserver = () => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      ScrollTrigger.refresh();
-    }, 250); // Debounce delay
-  };
-
-  window.addEventListener("resize", resizeObserver);
-
-    
-    return ()=>{
-      ScrollTrigger.getById("cardsScroll")?.kill();
-      clearTimeout(timeoutId);
-      window.removeEventListener("resize", resizeObserver);
+    if (elem.current) {
+      resizeObserver.observe(elem.current);
     }
+    // Add resize event listener
+  
+    // Cleanup function
+    return () => {
+      ScrollTrigger.getById("cardsScroll")?.kill();
+      if (elem.current) {
+        resizeObserver.unobserve(elem.current);
+      }
+      resizeObserver.disconnect();
+    };
   }, []);
+  
 
   const handleMouseEnter = (e: React.MouseEvent<HTMLElement>) => {
     if (cursorRef && cursorRef.current) {
@@ -144,8 +165,9 @@ const Home = () => {
             <TopCompanies />
           </div>
         </section>
-        <section >
-          <div className="w-full h-screen  bg-[#F1F0EE] " ref={elem} >
+        <section className="min-h-fit"  >
+          
+          <div className="w-full min-h-fit  bg-[#F1F0EE] "  ref={elem} >
             <Features />
           </div>
         </section>
